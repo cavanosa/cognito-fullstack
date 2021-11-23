f262709c63ca663c3dd31e722b948d9a9eced2c5
cavanosa <cavanosa@cavanosa.cav>
2021-11-23 20:58:38 +0100

    register

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
+import { Iuser } from './../models/iuser';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CognitoUserPool, CognitoUserAttribute } from 'amazon-cognito-identity-js';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -13,13 +17,45 @@ export class SignupComponent implements OnInit {
   password: string;
 
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
   onRegister(): void {
-
+    var poolData = {
+      UserPoolId: environment.UserPoolId, // Your user pool id here
+      ClientId: environment.ClientId, // Your client id here
+    };
+    var userPool = new CognitoUserPool(poolData);
+    // set attributes
+    var attrList = [];
+    var iuser: Iuser = {
+      email: this.email,
+      given_name: this.givenName,
+      nickname: this.nickname
+    }
+    for (let key in iuser) {
+      var attrData = {
+        Name: key,
+        Value: iuser[key]
+      }
+      var attr = new CognitoUserAttribute(attrData);
+      attrList.push(attr);
+    }
+    // sign up
+    userPool.signUp(this.email, this.password, attrList, [], (err, result) => {
+      if (err) {
+        alert(err.message || JSON.stringify(err));
+        return;
+      }
+      var newUser = result.user;
+      console.log(JSON.stringify(newUser));
+      alert('te hemos enviado un correo para activar tu cuenta');
+      this.router.navigate(['/login']);
+    });
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  UserPoolId: 'us-east-1_fXJtKQCyP',
+  ClientId: '1ks40rbtmor4uas19vqos16rqm'
 };
 
 /*
